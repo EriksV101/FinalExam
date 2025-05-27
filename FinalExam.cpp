4bdb8f3b5d274a8d851473ebf6a79b6901393d06
EriksV101 <Vanagseric@gmail.com>
2025-05-27 14:44:26 +0300

    V3 debuggin option 1

--- FinalExam.cpp
+++ FinalExam.cpp
@@ -4,13 +4,14 @@
 #include <iostream>
 using namespace std;
 
+const int X = 4, Y = 3;
+
 void adddata(double m[], double n);
 double sumpos(double m[]);
 
 int main()
 {
-	const int X = 4, Y = 3;
-	double matrix[X][Y], num;
+	double matrix[X][Y], num, sum = 0;
 	int choice, i = 0, j = 0;
 
 	do
@@ -23,21 +24,37 @@ int main()
 		if (choice == 1)
 		{
 			cout << "enter a number between -2.5 and 2.5" << endl;
-			cin >> num;
-			j++;
-			if (j == X)
+			while (i < Y)
 			{
-				j = 0;
-				i++;
-			}
-			if (i < X and i < Y)
-				adddata(matrix[j], num);
-			else
-				cerr << "The matrix is full" << endl;
+				cin >> num;
+				if (num >= -2.5 and num <= 2.5)
+				{
+					matrix[j][i] = num;
+					j++;
+					if (j == X)
+					{
+						j = 0;
+						i++;
+					}
+				}
+				else
+					cerr << "Incorrect number inputted" << endl;
+			}	
 		}
 		else if (choice == 2)
 		{
-			cout << "Sum num" << endl;
+			i = 0;
+			while (j < Y)
+			{
+				sum += sumpos(matrix[i]);
+				i++;
+				if (i == Y)
+				{
+					i = 0;
+
+				}
+			}
+			cout << "The sum of all positive numbers in the matrix is " << sum << endl;
 		}
 		else if (choice == 0)
 			;
@@ -46,7 +63,18 @@ int main()
 	} while (choice != 0);
 }
 
-void adddata(double& m, double n)
+void adddata(double m[], double n)
 {
-	m = n;
+}
+
+double sumpos(double m[])
+{
+	int i = 0;
+	double sum = 0;
+	while (i < X)
+	{
+		if (m[i] > 0)
+			sum += m[i];
+		i++;
+	}
 }
